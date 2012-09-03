9c7d20f1ff4bf0abbe65acdf9fba6607aa3cb751
Mate Lakat <mate.lakat@citrix.com>
2012-09-03 11:20:27 +0100

    Add logging, fixes #4

--- boxes/scripts/start_devstack.py
+++ boxes/scripts/start_devstack.py
@@ -1,10 +1,13 @@
 import sys
 import argparse
 import time
+import logging
 
 from boxes import Server, disconnect_all
 from boxes.scripts.get_devstack_domu_ip import get_devstack_ip
 
+
+logger = logging.getLogger(__name__)
 
 
 def command(xenhost, xenpass, devstackpass):
@@ -17,16 +20,23 @@ def command(xenhost, xenpass, devstackpass):
 
     devstack = Server(devstack_ip, 'stack', devstackpass)
 
+    logger.info("Waiting for devstack machine")
     devstack.wait_for_ssh()
 
+    logger.info("Checking run.sh results")
     if is_run_sh_succeeded(devstack):
-        return
+        logger.info("run.sh succeeded")
     else:
+        logger.info("run.sh failed, trying to fix it")
         if rabbit_is_failing(devstack):
+            logger.info("rabbitmq is not running, restarting it")
             restart_rabbit(devstack)
         time.sleep(5)
+        logger.info("Starting run.sh")
         start_run_sh(devstack)
         assert is_run_sh_succeeded(devstack)
+
+    logger.info("Devstack is up and running, disconnecting")
     disconnect_all()
 
 
@@ -59,6 +69,8 @@ def start_run_sh(devstack):
 
 
 def main():
+    logging.basicConfig(level=logging.WARNING)
+    logger.setLevel(logging.INFO)
     parser = argparse.ArgumentParser(description='wait for devstack')
     parser.add_argument('xenhost', help='XenServer host')
     parser.add_argument('xenpass', help='XenServer password')
