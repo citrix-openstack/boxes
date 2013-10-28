6d10b4b329c94618ab626b08bc83ea34110c0a4b
Mate Lakat <mate.lakat@citrix.com>
2013-10-28 22:07:14 +0000

    get_guest_ip implementation

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
             'create_start_deblike = boxes.scripts.create_start_deblike:main',
             'create_start_centos = boxes.scripts.create_start_centos:main',
             'install_openstack_plugins = boxes.scripts.install_openstack_plugins:main',
-            'get_devstack_domu_ip = boxes.scripts.get_devstack_domu_ip:main',
+            'get_guest_ip = boxes.scripts.get_guest_ip:main',
             'start_devstack = boxes.scripts.start_devstack:main',
             'run_tempest_tests = boxes.scripts.run_tempest_tests:main',
             'hard_reset = boxes.scripts.hard_reset:main',
