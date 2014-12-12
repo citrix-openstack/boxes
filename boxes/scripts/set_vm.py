7f417a78cb254a38ebab1a59e2322a27365ce852
Mate Lakat <mlakat@mlakat.hu>
2014-12-12 07:02:46 +0100

    Added util to set network

--- boxes/scripts/set_vm.py
+++ boxes/scripts/set_vm.py
@@ -6,6 +6,56 @@ from boxes.scripts import lib
 from boxes import Server
 
 
+def set_network(xenhost, vm_uuid, args):
+    network = xenhost.run(
+        'xe network-list'
+        ' name-label={network_name} --minimal'.format(
+            network_name=args.network_name))
+
+    if not network:
+        xenhost.run(
+            'xe network-create name-label={network_name}'.format(
+                network_name=args.network_name))
+
+    network = xenhost.run(
+        'xe network-list'
+        ' name-label={network_name} --minimal'.format(
+            network_name=args.network_name))
+
+    assert network
+
+    vif = xenhost.run(
+        'xe vif-list'
+        ' vm-uuid={vm_uuid}'
+        ' device={device} --minimal'.format(
+            vm_uuid=vm_uuid, device=args.device))
+
+    if vif:
+        network_of_vif = xenhost.run(
+            'xe vif-list'
+            ' vm-uuid={vm_uuid}'
+            ' device={device}'
+            ' params=network-uuid'
+            ' --minimal'.format(
+                vm_uuid=vm_uuid,
+                device=args.device))
+
+        if network_of_vif != network:
+            xenhost.run('xe vif-destroy uuid={vif}'.format(vif=vif))
+        else:
+            return
+
+    xenhost.run(
+        'xe vif-create'
+        ' network-uuid={network}'
+        ' device={device}'
+        ' vm-uuid={vm_uuid}'.format(
+            network=network,
+            device=args.device,
+            vm_uuid=vm_uuid)
+    )
+
+
 def set_disk(xenhost, vm_uuid, args):
     vdis = lib.vdis_of(xenhost, vm_uuid)
     if len(vdis) != 1:
@@ -54,8 +104,6 @@ def set_mac(xenhost, vm_uuid, args):
     )
 
 
-
-
 def set_vm(args):
     xenhost = Server(args.host, args.xsuser, args.xspass)
     xenhost.disable_known_hosts = True
@@ -89,5 +137,10 @@ def main():
     parser_mac = subparsers.add_parser('mac', help='set mac address')
     parser_mac.add_argument('mac_address', help='New MAC address')
     parser_mac.set_defaults(operation=set_mac)
+
+    parser_net = subparsers.add_parser('network', help='set network')
+    parser_net.add_argument('network_name', help='name of the network')
+    parser_net.add_argument('--device', help='device to set', type=int)
+    parser_net.set_defaults(operation=set_network)
 
     set_vm(parser.parse_args())
