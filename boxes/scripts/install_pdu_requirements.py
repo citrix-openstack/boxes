3cf6c38f5c21ef157922dbcd72d4bb0ce80c82c4
Mate Lakat <mlakat@mlakat.hu>
2014-12-17 12:13:47 +0100

    Fix installer hiding exit value

--- boxes/scripts/install_pdu_requirements.py
+++ boxes/scripts/install_pdu_requirements.py
@@ -1,10 +1,11 @@
 import subprocess
 import boxes
 import os
+import sys
 
 
 def main():
     boxes_path = os.path.dirname(os.path.abspath(boxes.__file__))
     pdu_requirements_path = os.path.join(boxes_path, "pdu-requirements.txt")
-    subprocess.call(["pip", "install", "-r", pdu_requirements_path])
+    sys.exit(subprocess.call(["pip", "install", "-r", pdu_requirements_path]))
 
